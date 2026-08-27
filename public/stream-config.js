0e5c9dbadba8b1fa41ff775cce2a49d8f58d991e
LogicalTrue <logicaltrue@users.noreply.github.com>
2026-08-27 16:15:37 -0300

    fix(stream): switch to optimized playlist.m3u8 for stable 3s HLS streaming without 404 part dropouts

--- public/stream-config.js
+++ public/stream-config.js
@@ -1,6 +1,6 @@
 export const STREAM_CONFIG = {
   kickChannel: 'blackozutr',
-  videoUrl: 'https://stream.blackozulive.com/app/stream/llhls.m3u8',
+  videoUrl: 'https://stream.blackozulive.com/app/stream/playlist.m3u8',
   offlinePoster: 'https://i.imgur.com/WZFLjFB.jpeg',
   onlinePoster: '',
   socials: {
