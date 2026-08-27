d93634f999c3d7fb861aea9af6c2937d9ed899d5
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2026-08-27 00:52:26 -0300

    feat(stream): connect to Hetzner MediaMTX Cloudflare stream endpoint

--- public/stream-config.js
+++ public/stream-config.js
@@ -1,6 +1,6 @@
 export const STREAM_CONFIG = {
   kickChannel: 'blackozutr',
-  videoUrl: 'https://livepeercdn.studio/hls/4197gut82w5c44pd/index.m3u8',
+  videoUrl: 'https://stream.blackozulive.com/live/stream/index.m3u8',
   offlinePoster: 'https://i.imgur.com/WZFLjFB.jpeg',
   onlinePoster: '',
   socials: {
