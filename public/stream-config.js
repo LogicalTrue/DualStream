0714483ffdd74677992b627408d2f5f51330879f
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2026-08-27 02:40:15 -0300

    feat(cdn): integrate BunnyCDN Pull Zone for scalable live streaming distribution

--- public/stream-config.js
+++ public/stream-config.js
@@ -1,6 +1,6 @@
 export const STREAM_CONFIG = {
   kickChannel: 'blackozutr',
-  videoUrl: 'wss://stream.blackozulive.com/app/stream',
+  videoUrl: 'https://stream-blackozu.b-cdn.net/app/stream/llhls.m3u8',
   offlinePoster: 'https://i.imgur.com/WZFLjFB.jpeg',
   onlinePoster: '',
   socials: {
