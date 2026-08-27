bc9145334e922a5a6dd003f29c6d7239be0cb438
LogicalTrue <logicaltrue@users.noreply.github.com>
2026-08-27 11:00:59 -0300

    test(streaming): set videoUrl to direct LL-HLS endpoint for pilot test

--- public/stream-config.js
+++ public/stream-config.js
@@ -1,6 +1,6 @@
 export const STREAM_CONFIG = {
   kickChannel: 'blackozutr',
-  videoUrl: 'wss://stream.blackozulive.com/app/stream',
+  videoUrl: 'https://stream.blackozulive.com/app/stream/llhls.m3u8',
   offlinePoster: 'https://i.imgur.com/WZFLjFB.jpeg',
   onlinePoster: '',
   socials: {
